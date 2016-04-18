a1b314d348d48f7f58b4b7b31625c9dd68bcfb86
amanjain <91aman@gmail.com>
2016-04-18 15:04:41 +0530

    added every over and every ball support. (cherry picked from commit aa83b9e) (cherry picked from commit 35fe90a)

--- src/js/LiveSports.jsx
+++ src/js/LiveSports.jsx
@@ -9,12 +9,34 @@ import MenuItem from 'material-ui/lib/menus/menu-item';
 import Toggle from 'material-ui/lib/toggle';
 import RaisedButton from 'material-ui/lib/raised-button';
 import CircularProgress from 'material-ui/lib/circular-progress';
+import DetailsModal from './components/DetailsModal/main.jsx';
+import Snackbar from 'material-ui/lib/snackbar';
 import ClassNames from 'classnames';
 import $ from 'jquery';
 import _ from 'lodash';
 
 
 let eventMap = {
+    EVERY_BALL: {
+        label: 'Every ball',
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/b/b0/Cricketball.png'
+    },
+    EVERY_OVER: {
+        label: 'End of over',
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/b/b0/Cricketball.png'
+    },
+    OUT: {
+        label: 'Wicket',
+        icon: 'http://www.nickjr.com/nickjr/buttons/alphabuttons/default-08-2015/icon-alpha-w-default-08-2015-2x.png'
+    },
+    SIX: {
+        label: '6 runs',
+        icon: 'http://christchurch.barnet.sch.uk/wp-content/uploads/2015/10/year-6-thumbnail.jpg'
+    },
+    FOUR: {
+        label: '4 runs',
+        icon: 'http://3.bp.blogspot.com/-9cSZuRMO65M/UOYn20oxGUI/AAAAAAAALgg/zTyw-uLPkO8/s1600/number4.jpg'
+    },
     "1 run": {
         label: '1 run',
         icon: 'https://creativenglishlearning.files.wordpress.com/2010/09/number1.jpg'
@@ -26,26 +48,6 @@ let eventMap = {
     "3 runs": {
         label: '3 runs',
         icon: 'https://weeklytreats.files.wordpress.com/2011/05/number3.jpg'
-    },
-    FOUR: {
-        label: '4 runs',
-        icon: 'http://3.bp.blogspot.com/-9cSZuRMO65M/UOYn20oxGUI/AAAAAAAALgg/zTyw-uLPkO8/s1600/number4.jpg'
-    },
-    SIX: {
-        label: '6 runs',
-        icon: 'http://christchurch.barnet.sch.uk/wp-content/uploads/2015/10/year-6-thumbnail.jpg'
-    },
-    OUT: {
-        label: 'Wicket',
-        icon: 'http://www.nickjr.com/nickjr/buttons/alphabuttons/default-08-2015/icon-alpha-w-default-08-2015-2x.png'
-    },
-    EVERY_OVER: {
-        label: 'End of over',
-        icon: 'https://upload.wikimedia.org/wikipedia/commons/b/b0/Cricketball.png'
-    },
-    EVERY_BALL: {
-        label: 'Every ball',
-        icon: 'https://upload.wikimedia.org/wikipedia/commons/b/b0/Cricketball.png'
     }
 };
 
@@ -94,7 +96,7 @@ function pushNotification(ball, content, eventarg) {
             body = 'Over complete. \n\n';
             break;
         default:
-            body = ball.players + ', ' + label;
+            body = ball.overs_actual + ', ' + ball.players + ', ' + label;
     }
 
     (event === 'OUT') && (body += '\n' + ball.dismissal);
@@ -119,6 +121,22 @@ function onMatchChange(event, index, value) {
     this.setState({selectedMatch: value})
 }
 
+function onDetailDialogClose() {
+    let that = this;
+
+    that.setState({
+        showDetails: false
+    })
+}
+
+function onSnackBarClose() {
+    let that = this;
+
+    that.setState({
+        showSnackbar: false
+    })
+}
+
 function onStartClick(event, index, value) {
     let that = this,
         refs = that.refs,
@@ -132,7 +150,7 @@ function onStartClick(event, index, value) {
     that.previousOver = undefined;
     that.previousBall = undefined;
 
-    that.setState({selectedEvents: selectedEvents, start: true});
+    that.setState({selectedEvents: selectedEvents, start: true, showSnackbar: true});
 }
 
 function onStopClick() {
@@ -149,7 +167,9 @@ class LiveSports extends Component {
             selectedMatch: undefined,
             selectedEvents: {},
             start: undefined,
-            loading: true
+            loading: true,
+            showDetails: false,
+            showSnackbar: false
         }
     }
 
@@ -184,6 +204,13 @@ class LiveSports extends Component {
                 })}>
                     <CircularProgress className="overlay-icon"/>
                 </div>
+                <DetailsModal open={state.showDetails} onClose={onDetailDialogClose.bind(that)}/>
+                <Snackbar
+                    open={state.showSnackbar}
+                    message="Congrats, You will receive your first notification soon."
+                    autoHideDuration={4000}
+                    onRequestClose={onSnackBarClose.bind(that)}
+                />
                 <div className="title">Live Sports</div>
                 <div className="body">
                     <div className="control-group">
@@ -223,55 +250,58 @@ class LiveSports extends Component {
             selectedEvents = state.selectedEvents;
 
         if (state.start) {
-            let intervalFunction = function () {
+            if (!that.intervalId) {
+                let intervalFunction = function () {
 
-                if (Notification.permission !== "granted") {
-                    Notification.requestPermission();
-                } else {
-                    $.getJSON('http://whateverorigin.org/get?url=' + encodeURIComponent('http://www.espncricinfo.com/ci/engine/match/' + state.selectedMatch + '.json') + '&callback=?', function (data) {
+                    if (Notification.permission !== "granted") {
+                        Notification.requestPermission();
+                    } else {
+                        $.getJSON('http://whateverorigin.org/get?url=' + encodeURIComponent('http://www.espncricinfo.com/ci/engine/match/' + state.selectedMatch + '.json') + '&callback=?', function (data) {
 
-                        let content = JSON.parse(data.contents),
-                            overs = content.comms || [],
-                            currentOver = overs[0] || {},
-                            currentOverNo = currentOver.over_number,
-                            balls = currentOver.ball || [],
-                            currentBall = (balls[0] || {}).overs_unique,
-                            iter,
+                            let content = JSON.parse(data.contents),
+                                overs = content.comms || [],
+                                currentOver = overs[0] || {},
+                                currentOverNo = currentOver.over_number,
+                                balls = currentOver.ball || [],
+                                currentBall = (balls[0] || {}).overs_unique,
+                                iter,
 
-                        //if no previous ball is set, set it as the current ball.
-                            previousBall = that.previousBall || currentBall,
-                            previousOver = that.previousOver || currentOverNo;
+                            //if no previous ball is set, set it as the current ball.
+                                previousBall = that.previousBall || currentBall,
+                                previousOver = that.previousOver || currentOverNo;
 
-                        that.previousBall = currentBall;
-                        that.previousOver = currentOverNo;
+                            that.previousBall = currentBall;
+                            that.previousOver = currentOverNo;
 
-                        for (iter = 0; iter < overs.length; iter++) {
-                            let over = overs[iter],
-                                balls = over.ball || [],
-                                j;
+                            for (iter = 0; iter < overs.length; iter++) {
+                                let over = overs[iter],
+                                    balls = over.ball || [],
+                                    j;
 
-                            for (j = 0; j < balls.length; j++) {
-                                let ball = balls[j];
+                                for (j = 0; j < balls.length; j++) {
+                                    let ball = balls[j];
 
-                                if (ball.overs_unique === previousBall) {
-                                    return;
-                                }
-                                console.log(ball.overs_unique, ball.event);
+                                    if (ball.overs_unique === previousBall) {
+                                        return;
+                                    }
+                                    console.log(ball.overs_unique, ball.event);
 
-                                (selectedEvents['EVERY_BALL'] || selectedEvents[ball.event]) && pushNotification.call(that, ball, content);
-                                if (selectedEvents['EVERY_OVER'] && currentOverNo != previousOver) {
-                                    pushNotification.call(that, ball, content, 'EVERY_OVER')
+                                    (selectedEvents['EVERY_BALL'] || selectedEvents[ball.event]) && pushNotification.call(that, ball, content);
+                                    if (selectedEvents['EVERY_OVER'] && currentOverNo != previousOver) {
+                                        pushNotification.call(that, ball, content, 'EVERY_OVER')
+                                    }
                                 }
                             }
-                        }
-                    });
-                }
-            };
+                        });
+                    }
+                };
 
-            intervalFunction();
-            that.intervalId = window.setInterval(intervalFunction, 30 * 1000);
+                intervalFunction();
+                that.intervalId = window.setInterval(intervalFunction, 15 * 1000);
+            }
         } else {
-            window.clearInterval(that.intervalId)
+            window.clearInterval(that.intervalId);
+            that.intervalId = undefined;
         }
     }
 
@@ -299,7 +329,7 @@ class LiveSports extends Component {
                 });
             });
 
-            this.setState({liveMatches, loading: false});
+            this.setState({liveMatches, loading: false, showDetails: true});
         });
     }
 }
